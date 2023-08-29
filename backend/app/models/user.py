8d43871d50f906aa805f0435fcb0ec68be592b70
Kamil Czarniecki <herrczarny@gmail.com>
2023-08-29 21:02:17 +0200

    fix: update on table constrains

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -3,6 +3,6 @@ from .. import db
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    parent_id = db.Column(db.Integer, nullable=False)
-    reputation = db.Column(db.Integer)
+    name = db.Column(db.String, nullable=False, unique=True)
+    parent_id = db.Column(db.Integer, nullable=True)
+    reputation = db.Column(db.Integer, nullable=True)
