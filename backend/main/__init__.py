9f194f9b9241c230391adc3c0cdb0fc3a716677c
Kamil Czarniecki <herrczarny@gmail.com>
2023-11-11 22:27:51 +0100

    feat: Flask-Migration added. Initial migration created

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 db = SQLAlchemy()
+migrate = Migrate(app, db)
 
 
 def create_app(db_init=False):
@@ -12,6 +14,7 @@ def create_app(db_init=False):
 
     with app.app_context():
         db.init_app(app)
+        migrate.init_app(app)
         if db_init:
             create_tables()
         from .routes.user_endpoints import user_endpoints
@@ -30,35 +33,37 @@ def create_tables():
 
     db.create_all()
 
-    db.session.add(IdentityType(name="human"))
-    db.session.add(IdentityType(name="ai"))
-    db.session.add(IdentityType(name="android"))
+    # TODO: External package with static data needs to be provided. Issue with double inserting values on dev server.
 
-    db.session.add(AffiliationList(name="Overseers"))
-    db.session.add(AffiliationList(name="Dizorders"))
-    db.session.add(AffiliationList(name="Hedonizers"))
-    db.session.add(AffiliationList(name="S.W.A.R.M"))
-    db.session.add(AffiliationList(name="DigiEvo"))
-
-    db.session.add(MessageStatus(name="sent"))
-    db.session.add(MessageStatus(name="received"))
-    db.session.add(MessageStatus(name="read"))
-    db.session.add(MessageStatus(name="error"))
-    db.session.add(MessageStatus(name="awaiting"))
-
-    db.session.add(GigCategoryNames(name="relation"))
-    db.session.add(GigCategoryNames(name="combat"))
-    db.session.add(GigCategoryNames(name="tech"))
-    db.session.add(GigCategoryNames(name="info"))
-    db.session.add(GigCategoryNames(name="courier"))
-    db.session.add(GigCategoryNames(name="medical"))
-    db.session.add(GigCategoryNames(name="hacking"))
-    db.session.add(GigCategoryNames(name="weapons"))
-    db.session.add(GigCategoryNames(name="drugs"))
-
-    db.session.add(GigStatus(name="available"))
-    db.session.add(GigStatus(name="in_progress"))
-    db.session.add(GigStatus(name="completed"))
-    db.session.add(GigStatus(name="pending"))
-
-    db.session.commit()
+    # db.session.add(IdentityType(name="human"))
+    # db.session.add(IdentityType(name="ai"))
+    # db.session.add(IdentityType(name="android"))
+    #
+    # db.session.add(AffiliationList(name="Overseers"))
+    # db.session.add(AffiliationList(name="Dizorders"))
+    # db.session.add(AffiliationList(name="Hedonizers"))
+    # db.session.add(AffiliationList(name="S.W.A.R.M"))
+    # db.session.add(AffiliationList(name="DigiEvo"))
+    #
+    # db.session.add(MessageStatus(name="sent"))
+    # db.session.add(MessageStatus(name="received"))
+    # db.session.add(MessageStatus(name="read"))
+    # db.session.add(MessageStatus(name="error"))
+    # db.session.add(MessageStatus(name="awaiting"))
+    #
+    # db.session.add(GigCategoryNames(name="relation"))
+    # db.session.add(GigCategoryNames(name="combat"))
+    # db.session.add(GigCategoryNames(name="tech"))
+    # db.session.add(GigCategoryNames(name="info"))
+    # db.session.add(GigCategoryNames(name="courier"))
+    # db.session.add(GigCategoryNames(name="medical"))
+    # db.session.add(GigCategoryNames(name="hacking"))
+    # db.session.add(GigCategoryNames(name="weapons"))
+    # db.session.add(GigCategoryNames(name="drugs"))
+    #
+    # db.session.add(GigStatus(name="available"))
+    # db.session.add(GigStatus(name="in_progress"))
+    # db.session.add(GigStatus(name="completed"))
+    # db.session.add(GigStatus(name="pending"))
+    #
+    # db.session.commit()
