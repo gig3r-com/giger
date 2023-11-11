2e56d22a8319ea1a5423c1929c32a3e32ae7b124
K.Cz <herrczarny@gmail.com>
2023-11-11 20:09:52 +0100

    Feature/gig delete (#21)

--- backend/main/db_models/gig.py
+++ backend/main/db_models/gig.py
@@ -1,5 +1,5 @@
-from typing import Dict, Set, List
-
+import logging
+from typing import Dict, List
 from .. import db
 from ..db_models.user import User
 
@@ -24,42 +24,7 @@ class Gig(db.Model):
     status = db.Column(db.ForeignKey(GigStatus.id))
     opened_by = db.Column(db.ForeignKey(User.id))  # TODO: What if user want to post it with different handle?
     taken_by = db.Column(db.ForeignKey(User.id), nullable=True)  # TODO: What if user want to use different handle?
-
-    @staticmethod
-    def create_gig(data) -> int:
-
-        category = data["category"]
-
-        if category is not int:
-
-            category_id = GigCategoryNames.query.filter_by(name=category).first()
-        else:
-            category_id = data["category"]
-
-        if category_id:
-            data["category"] = category_id.id
-        else:
-            raise ValueError(f"No such category: {category}")
-
-        new_gig = Gig(**data)
-        db.session.add(new_gig)
-        db.session.commit()
-
-        return new_gig.id
-
-    @staticmethod
-    def allowed_params() -> List:
-        return [
-            "id",
-            "payout",
-            "title",
-            "description",
-            "category",
-            "reputation_required",
-            "status",
-            "opened_by",
-            "taken_by"
-        ]
+    archived = db.Column(db.Boolean, default=False)
 
     def to_dict(self) -> Dict:
         return {
@@ -71,9 +36,54 @@ class Gig(db.Model):
             "reputation_required": self.reputation_required,
             "status": self.status,
             "opened_by": self.opened_by,
-            "taken_by": self.taken_by
+            "taken_by": self.taken_by,
+            "archived": self.archived
         }
+
+
+def allowed_params() -> List:
+    return [
+        "id",
+        "payout",
+        "title",
+        "description",
+        "category",
+        "reputation_required",
+        "status",
+        "opened_by",
+        "taken_by"
+    ]
+
+
+def create_gig(data) -> int:
+    category = data["category"]
+
+    if category is not int:
+        category_id = GigCategoryNames.query.filter_by(name=category).first()
+    else:
+        category_id = data["category"]
+
+    if category_id:
+        data["category"] = category_id.id
+    else:
+        raise ValueError(f"No such category: {category}")
+
+    new_gig = Gig(**data)
+    db.session.add(new_gig)
+    db.session.commit()
+
+    return new_gig.id
 
 
 def get_all_gigs():
     return Gig.query.all()
+
+
+def get_gig_by_id(gig_id: int):
+    return Gig.query.filter_by(id=gig_id, archived=False).first()
+
+
+def delete_gig_by_id(gig_id: int):
+    condition = Gig.id == gig_id
+    db.session.query(Gig).filter(condition).update({"archived": True})
+    db.session.commit()
