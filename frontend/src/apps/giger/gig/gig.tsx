89f5a21b057ca5beb5e915bce85819aec808f1e6
Borys Juskiw <juskiw@ebu.ch>
2023-12-08 22:34:29 +0100

    fix: gig opening

--- frontend/src/apps/giger/gig/gig.tsx
+++ frontend/src/apps/giger/gig/gig.tsx
@@ -52,7 +52,7 @@ export const Gig: FC<IGigProps> = ({ gig, selectedId, delayMultiplier }) => {
                 fetchConvo(gig.id);
             }
         },
-        [selectedId, gig, fetchConvo]
+        [selectedId, gig]
     );
 
     return (
