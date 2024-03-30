5ead70a6532ef796ec1792338730fb86c71d76ee
Jakub Zielnik <jakubzielnik@gmail.com>
2024-03-30 20:23:09 +0100

    Added new endpoints in NetworkController

--- backendDotnet/Giger/Controllers/NetworksController.cs
+++ backendDotnet/Giger/Controllers/NetworksController.cs
@@ -1,6 +1,8 @@
 ﻿using Giger.Models.Networks;
+using Giger.Models.User;
 using Giger.Services;
 using Microsoft.AspNetCore.Mvc;
+using OperatingSystem = Giger.Models.Networks.OperatingSystem;
 
 namespace Giger.Controllers
 {
@@ -10,11 +12,262 @@ namespace Giger.Controllers
     {
         private readonly NetworksService _networkService = networkService;
 
-        [HttpGet("all")]
-        public async Task<List<Network>> GetNetworks() => await _networkService.GetAllNetworksAsync();
+        #region Networks
+        [HttpGet("network")]
+        public async Task<ActionResult<Network>> GetNetwork(string id)
+        {
+            var network = await _networkService.GetNetworkByIdAsync(id);
+            if (!IsAuthorized(network?.AdminId))
+            {
+                return Forbid();
+            }
+            return network;
+        }
 
+        [HttpGet("network/name")]
+        public async Task<ActionResult<string>> GetNetworkName(string id)
+        {
+            var network = await _networkService.GetNetworkByIdAsync(id);
+            if (!IsAuthorized(network?.AdminId))
+            {
+                return Forbid();
+            }
+            return network.Name;
+        }
+
+        #endregion
+
+        #region Subnetworks
         [HttpGet("subnetwork/all")]
         public async Task<List<Subnetwork>> GetSubnetworks() => await _networkService.GetAllSubnetworksAsync();
 
+        [HttpPost("subnetwork")]
+        public async Task<IActionResult> PostSubnetwork(Subnetwork newSubnetwork)
+        {
+            var network = await _networkService.GetNetworkByIdAsync(newSubnetwork.NetworkId);
+            if (IsAuthorized(network?.AdminId) == false)
+            {
+                return Forbid();
+            }
+            await _networkService.CreateSubnetworkAsync(newSubnetwork);
+            return CreatedAtAction(nameof(PostSubnetwork), new { id = newSubnetwork.Id }, newSubnetwork);
+        }
+
+        [HttpPatch("subnetwork/firewall")]
+        public async Task<IActionResult> ChangeFirewall(string subnetworkId, string firewall)
+        {
+            var result = await ValidateSubnetworkRequest(subnetworkId);
+            if (result.Result is not OkResult)
+            {
+                return result.Result;
+            }
+
+            var subnetwork = result.Subnetwork;
+            if (Enum.TryParse<Firewall>(firewall, out var firewallEnum))
+            {
+                subnetwork.Firewall = firewallEnum;
+                await _networkService.UpdateSubnetworkAsync(subnetworkId, subnetwork);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPatch("subnetwork/os")]
+        public async Task<IActionResult> ChangeOS(string subnetworkId, string os)
+        {
+            var result = await ValidateSubnetworkRequest(subnetworkId);
+            if (result.Result is not OkResult)
+            {
+                return result.Result;
+            }
+
+            var subnetwork = result.Subnetwork;
+            if (Enum.TryParse<OperatingSystem>(os, out var osEnum))
+            {
+                subnetwork.OperatingSystem = osEnum;
+                await _networkService.UpdateSubnetworkAsync(subnetworkId, subnetwork);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("subnetwork/ice")]
+        public async Task<IActionResult> AddIce(string subnetworkId, string ice)
+        {
+            var result = await ValidateSubnetworkRequest(subnetworkId);
+            if (result.Result is not OkResult)
+            {
+                return result.Result;
+            }
+
+            var subnetwork = result.Subnetwork;
+            if (Enum.TryParse<Ice>(ice, out var iceEnum))
+            {
+                if (subnetwork.Ice.Contains(iceEnum))
+                {
+                    return BadRequest($"Subnetwork already contains {ice} ICE.");
+                }
+                subnetwork.Ice = [.. subnetwork.Ice, iceEnum ];
+                await _networkService.UpdateSubnetworkAsync(subnetworkId, subnetwork);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest("Unsupported ICE");
+            }
+        }
+
+        [HttpDelete("subnetwork/ice")]
+        public async Task<IActionResult> RemoveIce(string subnetworkId, string ice)
+        {
+            var result = await ValidateSubnetworkRequest(subnetworkId);
+            if (result.Result is not OkResult)
+            {
+                return result.Result;
+            }
+            var subnetwork = result.Subnetwork;
+            if (Enum.TryParse<Ice>(ice, out var iceEnum))
+            {
+                if (subnetwork.Ice.Contains(iceEnum))
+                {
+                    return BadRequest($"Subnetwork already contains {ice} ICE.");
+                }
+                subnetwork.Ice = subnetwork.Ice.Except([iceEnum]).ToArray();
+                await _networkService.UpdateSubnetworkAsync(subnetworkId, subnetwork);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest("Unsupported ICE");
+            }
+        }
+
+        //TODO: this endpoint needs to be thought through if its needed and what to return
+        [HttpGet("subnetwork/user/all")]
+        public async Task<ActionResult<string[]>> GetAllUsers(string subnetworkId)
+        {
+            var result = await ValidateSubnetworkRequest(subnetworkId);
+            switch (result.Result)
+            {
+                case OkResult:
+                    break;
+                case NotFoundResult:
+                    return NotFound();
+                case BadRequestResult:
+                    return BadRequest();
+                case ForbidResult:
+                    return Forbid();
+                default:
+                    return BadRequest();
+            }
+
+            return result.Subnetwork.Users;
+        }
+        
+        [HttpGet("subnetwork/user")]
+        public async Task<ActionResult<UserPrivate>> GetUser(string subnetworkId, string userId)
+        {
+            var result = await ValidateSubnetworkRequest(subnetworkId);
+            switch (result.Result)
+            {
+                case OkResult:
+                    break;
+                case NotFoundResult:
+                    return NotFound();
+                case BadRequestResult:
+                    return BadRequest();
+                case ForbidResult:
+                    return Forbid();
+                default:
+                    return BadRequest();
+            }
+
+            var subnetwork = result.Subnetwork;
+            var user = await _userService.GetAsync(userId);
+            if (user is null)
+            {
+                return NotFound("No such user.");
+            }
+            if (!subnetwork.Users.Contains(userId))
+            {
+                return Forbid("No such user in this subnetwork.");
+            }
+            return user;
+        }
+
+        [HttpPut("subnetwork/users")]
+        public async Task<IActionResult> AddUser(string subnetworkId, string userId)
+        {
+            var result = await ValidateSubnetworkRequest(subnetworkId);
+            if (result.Result is not OkResult)
+            {
+                return result.Result;
+            }
+
+            var subnetwork = result.Subnetwork;
+            var user = await _userService.GetAsync(userId);
+            if (user is null)
+            {
+                return NotFound("No such user.");
+            }
+            if (subnetwork.Users.Contains(userId))
+            {
+                return Ok("This user was already in that subnetwork.");
+            }
+            subnetwork.Users = [.. subnetwork.Users, userId];
+            await _networkService.UpdateSubnetworkAsync(subnetworkId, subnetwork);
+            return Ok();
+        }
+
+        [HttpDelete("subnetwork/user")]
+        public async Task<IActionResult> RemoveUser(string subnetworkId, string userId)
+        {
+            var result = await ValidateSubnetworkRequest(subnetworkId);
+            if (result.Result is not OkResult)
+            {
+                return result.Result;
+            }
+            var subnetwork = result.Subnetwork;
+            var user = await _userService.GetAsync(userId);
+            if (user is null)
+            {
+                return NotFound("No such user.");
+            }
+            if (!subnetwork.Users.Contains(userId))
+            {
+                return Ok();
+            }
+            await _networkService.UpdateSubnetworkAsync(subnetworkId, subnetwork);
+            return Ok();
+        }
+        #endregion
+
+        #region HelperMethods
+        private async Task<(IActionResult Result, Subnetwork? Subnetwork)> ValidateSubnetworkRequest(string subnetworkId)
+        {
+            var subnetwork = await _networkService.GetSubnetworkByIdAsync(subnetworkId);
+            if (subnetwork is null)
+            {
+                return (NotFound(), null);
+            }
+            var network = await _networkService.GetNetworkByIdAsync(subnetwork.NetworkId);
+            if (network is null)
+            {
+                return (BadRequest(), null);
+            }
+            if (!IsAuthorized(network.AdminId))
+            {
+                return (Forbid(), null);
+            }
+
+            return (Ok(), subnetwork);
+        }
+        #endregion
     }
 }
