91e66d0b10193027b4853443728da590252a3f27
Jakub Zielnik <jakubzielnik@gmail.com>
2024-04-08 17:14:41 +0200

    Removed redundant GigBase. Now merged into Gig

--- backendDotnet/Giger/Models/GigModels/Gig.cs
+++ backendDotnet/Giger/Models/GigModels/Gig.cs
@@ -3,8 +3,23 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace Giger.Models.GigModels
 {
-    public class Gig : GigBase
+    public class Gig
     {
+        [BsonId]
+        public string Id { get; set; }
+
+        public decimal Payout { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [BsonRepresentation(BsonType.String)]
+        public GigCategoryNames Category { get; set; }
+
+        public GigRepuationLevels? RepurationRequired { get; set; }
+
+        public bool? AnonymizedAuthor { get; set; }
         [BsonRepresentation(BsonType.String)]
         public GigStatus Status { get; set; }
 
