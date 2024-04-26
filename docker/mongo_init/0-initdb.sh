e1721f14e54788a7377b90dbd47b71e7a2c59a4c
mivalsten <mivalsten@gmail.com>
2024-04-26 22:30:56 +0200

    Fix giger db in docker (#69)
    Fixes mongo configuration, which is a stupid database with stupid
    configuration.

--- docker/mongo_init/0-initdb.sh
+++ docker/mongo_init/0-initdb.sh
@@ -1,17 +1,10 @@
 mongosh <<EOF
 
+db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
 db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE).auth(
     process.env.MONGO_INITDB_ROOT_USERNAME,
     process.env.MONGO_INITDB_ROOT_PASSWORD
 );
-db = db.getSiblingDB(process.env.GIGER_DATABASE_NAME)
-db.createUser({
-  user: process.env.GIGER_USERNAME,
-  pwd: process.env.GIGER_USERNAME,
-  roles: [ { role: "userAdminAnyDatabase", db: process.env.GIGER_DATABASE_NAME }, 
-             { role: "dbAdminAnyDatabase", db: process.env.GIGER_DATABASE_NAME }, 
-             { role: "readWriteAnyDatabase", db: process.env.GIGER_DATABASE_NAME } ]
-});
 db.createCollection('Accounts')
 db.createCollection('Anonymized')
 db.createCollection('Auths')
