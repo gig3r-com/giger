982bdf8028525246fdfd969d0f73f738995cbdbe
Jakub Zielnik <jakubzielnik@gmail.com>
2024-05-07 18:02:21 +0200

    Fixed setting stats

--- backendDotnet/Giger/Models/User/Stats/CyberwareLevel.cs
+++ backendDotnet/Giger/Models/User/Stats/CyberwareLevel.cs
@@ -28,6 +28,10 @@
                 {
                     _stat = MAXVALUE;
                 }
+                else
+                {
+                    _stat = value;
+                }
             }
         }
 
