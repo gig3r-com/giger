49eb9a818fb4461f39f62fc8e22de1763a2d72eb
Jakub Zielnik <jakubzielnik@gmail.com>
2024-05-20 22:20:39 +0200

    Update UserSimple.cs

--- backendDotnet/Giger/Models/User/UserSimple.cs
+++ backendDotnet/Giger/Models/User/UserSimple.cs
@@ -63,6 +63,7 @@ namespace Giger.Models.User
             CombatSkill = user.CombatSkill;
             VibeFunction = user.VibeFunction;
             VibeEngagement = user.VibeEngagement;
+            VibeOpinions = "";
             FavoriteUserIds = user.FavoriteUserIds;
             Faction = user.Faction;
             FactionRankActual = user.FactionRankActual;
