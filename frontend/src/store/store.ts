9b350537284b9a7981a278e285f045b1d7cc4ccb
Borys Juskiw <juskiw.borys@gmail.com>
2024-05-24 22:35:06 +0200

    fix: listener

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -21,3 +21,4 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+export type StoreType = typeof store;
