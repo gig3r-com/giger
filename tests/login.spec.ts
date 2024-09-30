de4061cbab0d4babfe77168e46b3e06c3fb37d75
SBielawska <s.d.bielawska@gmail.com>
2024-09-30 19:57:02 +0200

    First test in Playwright

---
+++ tests/login.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("UserLogin", () => {
+  test("SuccesfullLogin", async ({ page }) => {
+    await page.goto("http://localhost:8080/");
+    await page.getByPlaceholder("Username").fill("0_connor");
+    await page.getByPlaceholder("Username").press("Tab");
+    await page.getByPlaceholder("Password").fill("chip");
+    await page.getByRole("button", { name: "Log in" }).click();
+    await page.click('//a[text()="my.id"]');
+
+    await expect(page.getByText("O'connor")).toHaveText("O'connor");
+  });
+});
+
+test("UnsuccesfullLogin", async ({ page }) => {
+  await page.goto("http://localhost:8080/");
+  await page.getByPlaceholder("Username").fill("O_connor");
+  await page.getByPlaceholder("Password").fill("chip");
+  await page.getByRole("button", { name: "Log in" }).click();
+
+  await expect(
+    page.getByText("Wrong username or password provided")
+  ).toHaveText("Wrong username or password provided");
+});
