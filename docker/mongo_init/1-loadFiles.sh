7fdd4e2a87f68d40b3eba47daafdade108068e75
Grzegorz Kawka-Osik <grzegorz@kawka-osik.eu>
2025-02-09 00:58:39 +0100

    update mongo init files

--- docker/mongo_init/1-loadFiles.sh
+++ docker/mongo_init/1-loadFiles.sh
@@ -1,12 +1,24 @@
 for COL in `ls /data/*.json`; do
-    echo "Processing $COL file..."
-    mongoimport --host localhost \
-                --db $MONGO_INITDB_DATABASE \
-                --collection $(basename $COL .json) \
-                --type json \
-                --file $COL \
-                --jsonArray \
-                --authenticationDatabase $MONGO_INITDB_DATABASE \
-                --username $MONGO_INITDB_ROOT_USERNAME \
-                --password $MONGO_INITDB_ROOT_PASSWORD
+    COLLECTION_NAME=$(basename $COL .json)
+    COUNT=$(mongo   --quiet \
+                    --host localhost \
+                    --authenticationDatabase $MONGO_INITDB_DATABASE \
+                    --username $MONGO_INITDB_ROOT_USERNAME \
+                    --password $MONGO_INITDB_ROOT_PASSWORD \
+                    --eval "db.getSiblingDB('$MONGO_INITDB_DATABASE').$COLLECTION_NAME.count()")
+
+    if [ "$COUNT" -eq 0 ]; then
+        echo "Processing $COL file..."
+        mongoimport --host localhost \
+                    --db $MONGO_INITDB_DATABASE \
+                    --collection $COLLECTION_NAME \
+                    --type json \
+                    --file $COL \
+                    --jsonArray \
+                    --authenticationDatabase $MONGO_INITDB_DATABASE \
+                    --username $MONGO_INITDB_ROOT_USERNAME \
+                    --password $MONGO_INITDB_ROOT_PASSWORD
+    else
+        echo "Skipping $COLLECTION_NAME as it already has data."
+    fi
 done
